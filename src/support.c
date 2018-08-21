3dcb952d2c392d5656ccba48ab7eb125b3d75917
Mauro Carvalho Chehab <mchehab+samsung@kernel.org>
2018-08-21 13:08:56 -0300

    support: get rid of create_pixbuf()
    This function is not used anymore. Get rid of it.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- src/support.c
+++ src/support.c
@@ -52,32 +52,6 @@ GtkWidget *lookup_widget (GtkWidget * widget, const gchar * widget_name)
         g_warning ("Widget not found: %s", widget_name);
     return found_widget;
 }
-GdkPixbuf *create_pixbuf (const gchar * filename)
-{
-    gchar *pathname = NULL;
-    GdkPixbuf *pixbuf;
-    GError *error = NULL;
-
-    if (!filename || !filename[0])
-        return NULL;
-
-    pathname =
-        gnome_program_locate_file (NULL, GNOME_FILE_DOMAIN_APP_PIXMAP,
-                                   filename, TRUE, NULL);
-
-    if (!pathname) {
-        g_warning (_("Couldn't find pixmap file: %s"), filename);
-        return NULL;
-    }
-    pixbuf = gdk_pixbuf_new_from_file (pathname, &error);
-    if (!pixbuf) {
-        fprintf (stderr, "Failed to load pixbuf file: %s: %s\n", pathname,
-                 error->message);
-        g_error_free (error);
-    }
-    g_free (pathname);
-    return pixbuf;
-}
 
 int error_dialog (char *message)
 {
